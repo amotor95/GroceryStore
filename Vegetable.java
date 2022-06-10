bc56d2fd322d25209968ee079b2d3490e3b421bb
amotor95 <59101078+amotor95@users.noreply.github.com>
2022-06-10 09:56:59 -0400

    Add files via upload

--- Vegetable.java
+++ Vegetable.java
@@ -1,36 +1,73 @@
-
-public class Vegetable extends FoodItem
-{
+public class Vegetable extends FoodItem {
+	
 	private double pricePerPound;
-	private int temperatureKeptAt;
+	private int tempretureKeptAt;
 	private int daysExpired;
 	
-	public Vegetable(boolean frozen, int daysTillExpiration, double costPerPound, int temperatureKeptAt, int daysAfterExpiration) 
-	{
+	public Vegetable(boolean frozen, int daysTillExpiration, double costPerPound, int tempretureKeptAt, boolean canned, int daysAfterExpiration) {
+		
 		super(true, frozen, daysTillExpiration);
 		this.pricePerPound = costPerPound;
-		this.temperatureKeptAt = temperatureKeptAt;
 		this.daysExpired = daysAfterExpiration;
+		
+		
 	}
 	
 	
 	
-	public double calculatePrice(double pounds) 
-	{
+	public double calculateprice(double pounds) {
+		
 		return pricePerPound * pounds;
+		
 	}
 	
 	
-	public boolean safeToEat() 
-	{
-		if (temperatureKeptAt <= 32 && daysExpired <= 240)
+	public boolean safetoeat() {
+		
+		int daysTillExpiration = getExpirationTime();
+		
+		if(isFrozen()) {
+			
+			if((tempretureKeptAt <= 32) && (daysExpired <= 240)) {
+				
 				return true;
-		else 
-		{ 
-			if (daysExpired > 0)
+				
+			}
+			
+			else {
+				
 				return false;
-			else
-				return true;
+				
+			}
+			
 		}
+		
+		else {
+			
+			if(daysTillExpiration == 0) {
+				
+				return false;
+			}
+			
+			
+			else {
+				return true;
+			}
+			
+			
+		}
+		
+		
+		
+		
 	}
+
+
+
+	@Override
+	public double calculatePrice(double foodUnit) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
