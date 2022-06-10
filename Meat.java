3d3d18f2df68b10e17a4237908c8569900e06472
amotor95 <59101078+amotor95@users.noreply.github.com>
2022-06-10 09:16:59 -0400

    Add files via upload

--- Meat.java
+++ Meat.java
@@ -11,5 +11,16 @@ public class Meat extends FoodItem {
 	public double calculatePrice(double pounds) {
 		return pounds * pricePerLb;
 	}
+	
+	public boolean expired(int daysPassed) {
+		if(frozen) {
+			if(daysPassed < this.timeTillExpiration*10) {
+				return false;
+			}
+		} else if(daysPassed < this.timeTillExpiration) {
+			return false;
+		}
+		return true;
+	}
 
 }
